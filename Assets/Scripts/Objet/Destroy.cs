e9703e8a07e511177dbd04317b032fa5e7d62a37
Lucas Pécout <lucas.pecout@e-artsup.net>
2023-12-13 15:42:14 +0100

    random spawn

--- Assets/Scripts/Objet/Destroy.cs
+++ Assets/Scripts/Objet/Destroy.cs
@@ -4,23 +4,22 @@ using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class destroy : MonoBehaviour
+public class Destroy : MonoBehaviour
 {
-    [SerializeField] private Vector3 position;
     public int poid;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        position = transform.position;
-
-        if (position.y < -10) { 
-            Destroy(gameObject); 
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
         }
     }
 }
