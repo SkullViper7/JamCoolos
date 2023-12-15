483cc3dc02406dee2d4f31e5040bda8ff9d35926
Posey50 <leolecointre50@gmail.com>
2023-12-15 10:37:09 +0100

    petits correctifs sur le spawn des objets

--- Assets/Scripts/Game/Objects/CollectableObject.cs
+++ Assets/Scripts/Game/Objects/CollectableObject.cs
@@ -4,38 +4,35 @@ using UnityEngine;
 
 public class CollectableObject : MonoBehaviour
 {
-    private MeshRenderer myMeshRenderer;
-    private MeshFilter myFilter;
-    private MeshCollider myCollider;
-
-
+    private MeshRenderer meshRenderer;
+    private MeshFilter meshFilter;
+    private MeshCollider meshCollider;
 
     public int weight;
-    public int spawnProba;
+    public int probability;
     public int score;
-    public CollectableObjectBase myObject;
+    public CollectableObjectBase collectableObjectBase;
 
-    
-
-    public void Imoinitialized()
+    public void InitialiseObject()
     {
-        weight = myObject.weight;
-        spawnProba = myObject.spawnProba;
-        score = myObject.score;
-        myMeshRenderer = GetComponent<MeshRenderer>();
-        myMeshRenderer.material = myObject.material;
-        myFilter = GetComponent<MeshFilter>();
-        myFilter.mesh = myObject.mesh;
-        myCollider = GetComponent<MeshCollider>();
-        myCollider.sharedMesh = myObject.mesh;
+        //Set up object
+        weight = collectableObjectBase.weight;
+        probability = collectableObjectBase.spawnProba;
+        score = collectableObjectBase.score;
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material = collectableObjectBase.material;
+        meshFilter = GetComponent<MeshFilter>();
+        meshFilter.mesh = collectableObjectBase.mesh;
+        meshCollider = GetComponent<MeshCollider>();
+        meshCollider.sharedMesh = collectableObjectBase.mesh;
     }
 
     public void Update()
     {
+        //Destoy object when it falls under the map
         if (transform.position.y < -10)
         {
             Destroy(gameObject);
         }
     }
-
 }
