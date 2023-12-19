b4c1bd1938be69b8a4193e6d9f198065fa7bfccb
Viper <41963246+SkullViper7@users.noreply.github.com>
2023-12-19 10:57:27 +0100

    punch sfx

--- Assets/Scripts/Game/Player/PushOtherPlayers.cs
+++ Assets/Scripts/Game/Player/PushOtherPlayers.cs
@@ -8,11 +8,15 @@ public class PushOtherPlayers : MonoBehaviour
     private PlayerPerimeter playerPerimeter;
     Animator animator;
 
+    AudioSource audioSource;
+    public List<AudioClip> punchSFX;
+
     void Start()
     {
         playerPerimeter = GetComponentInChildren<PlayerPerimeter>();
         animator = GetComponentInChildren<Animator>();
         animator.SetLayerWeight(1, 1);
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void TryToPush()
@@ -50,6 +54,8 @@ public class PushOtherPlayers : MonoBehaviour
 
     public void Push(PlayerStateMachine _stateMachine)
     {
+        int randomSFX = Random.Range(0, punchSFX.Count);
+        audioSource.PlayOneShot(punchSFX[randomSFX]);
         //Indicate to the other player that this player is the player who has pushed him
         _stateMachine.GetComponent<PlayerFall>().playerThatPushedMe = this.gameObject;
 
